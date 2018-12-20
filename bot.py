498c3888ffc6873c2f894f1012e4a62d78f3a29f
Jihee Lee <jiheelee.ljh@gmail.com>
2018-12-20 08:22:14 +0000

    jizzangbot 생성

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+token = os.getenv('TELE_TOKEN')
+method = 'getUpdates'
+url = "https://api.hphk.io/telegram/bot{}/{}".format(token,method)
+
+res = requests.get(url).json()
+
+user_id = res["result"][0]["message"]["from"]["id"]
+msg = "야호"
+
+method = 'sendMessage'
+msg_url = "https://api.hphk.io/telegram/bot{}/{}?chat_id={}&text={}".format(token,method,user_id,msg)
+print(msg_url)
+requests.get(msg_url)
+
